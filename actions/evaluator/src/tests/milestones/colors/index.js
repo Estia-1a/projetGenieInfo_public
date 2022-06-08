9b17e1338a6862511f5264eae00d6f9e90b749a0
dhmmasson <5263585+dhmmasson@users.noreply.github.com>
2022-06-09 01:53:31 +0200

    [Evaluator] Remove old test generation

--- actions/evaluator/src/tests/milestones/colors/index.js
+++
@@ -1,4 +0,0 @@
-import blue from  "./blue.js" ;
-const statistiques = [ ...blue ];
-statistiques.forEach( e=> e.milestone = "colors");
-export default statistiques ;
--- actions/evaluator/src/tests/milestones/tutorial/index.js
+++
@@ -1,8 +0,0 @@
-import dimension from  "./dimensions.js" ;
-import first_pixel from "./firstPixel.js"
-import tenth_pixel from "./tenthPixel.js"
-import second_line from "./secondLine.js"
-import print_pixel from "./printPixel.js"
-const tutorial = [ ...dimension, ...first_pixel, ...tenth_pixel, ...second_line, ...print_pixel  ];
-tutorial.forEach( e=> e.milestone = "Tutorial");
-export default tutorial ;
