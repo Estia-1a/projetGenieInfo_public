516623a37fb9142849163a2300e140c7e5c488ad
Dimitri Masson <5263585+dhmmasson@users.noreply.github.com>
2022-06-13 13:12:30 +0000

    [ClassAdmin] Octokit init

--- packages/classAdministrator/dist/index.js
+++ packages/classAdministrator/dist/index.js
@@ -1,2 +1,45 @@
-"use strict";
-console.log("hello, world");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { config } from "dotenv";
+import { Octokit } from "octokit";
+/**
+ *
+ * @param mode Which mode ro run
+ */
+function run(mode) {
+    return __awaiter(this, void 0, void 0, function* () {
+        init();
+        switch (mode) {
+            case "test":
+                test();
+                break;
+            case "rooster":
+                yield getRooster();
+        }
+    });
+}
+function getRooster() {
+    return __awaiter(this, void 0, void 0, function* () {
+        return true;
+    });
+}
+function test() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+        const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+        // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+        const { data: { login }, } = yield octokit.rest.users.getAuthenticated();
+        console.log("Hello, %s", login);
+    });
+}
+function init() {
+    config();
+}
+run("test");
--- packages/classAdministrator/src/index.ts
+++ packages/classAdministrator/src/index.ts
@@ -1,1 +1,37 @@
-console.log("hello, world");
+import { config } from "dotenv";
+import { Octokit } from "octokit";
+
+/**
+ *
+ * @param mode Which mode ro run
+ */
+async function run(mode: string) {
+  init();
+  switch (mode) {
+    case "test":
+      await test();
+      break;
+    case "rooster":
+      await getRooster();
+  }
+}
+
+async function getRooster() {
+  return true;
+}
+
+async function test() {
+  // Hydrate Octokit 
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
+  const {
+    data: { login },
+  } = await octokit.rest.users.getAuthenticated();
+  console.log("Hello, %s", login);
+}
+
+function init() {
+  config();
+}
+run("test");
