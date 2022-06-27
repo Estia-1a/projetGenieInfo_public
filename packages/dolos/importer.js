67b9d77aedcbd7568dfb60d8408e171b9abf818a
Dimitri Masson <5263585+dhmmasson@users.noreply.github.com>
2022-06-27 12:58:12 +0000

    Add source importer, update similarity clustering

--- packages/dolos/importer.js
+++ packages/dolos/importer.js
@@ -11,12 +11,13 @@ const students = {} ;
 const cleanPath = ( path ) => path?.replace( "result/" , "" ).replace(".js", "")
 
 data.map( e => {
-    const l = students[ cleanPath(e.leftFilePath) ] = students[ cleanPath(e.leftFilePath) ]  ?? { triche : false, cluster : null, link : 0 , similarity : 0  }
-    const r = students[ cleanPath(e.rightFilePath) ] = students[ cleanPath(e.rightFilePath) ]  ?? { triche : false, cluster : null, link : 0 , similarity : 0  }
-    if( e.similarity > 0.40 ) {
+    const l = students[ cleanPath(e.leftFilePath) ] = students[ cleanPath(e.leftFilePath) ]  ?? { triche : false, cluster : null, link : 0 , similarity : 0  , all:{}}
+    const r = students[ cleanPath(e.rightFilePath) ] = students[ cleanPath(e.rightFilePath) ]  ?? { triche : false, cluster : null, link : 0 , similarity : 0 , all:{} }
+    if( e.similarity > 0.35 ) {
         cluster.union( cleanPath(e.leftFilePath), cleanPath(e.rightFilePath) )
         l.triche = true ;
         l.link++ 
+        l.all[cleanPath(e.rightFilePath)] = e.similarity
         if(e.similarity > l.similarity ) {
             l.similarity = Math.max( l.similarity , e.similarity )
             l.bestMatch = cleanPath(e.rightFilePath)
@@ -27,6 +28,7 @@ data.map( e => {
         }
         r.triche = true ;
         r.link++ 
+        r.all[cleanPath(e.leftFilePath)] = e.similarity
     }
 })
 
@@ -34,15 +36,10 @@ data.map( e => {
 Object.entries( students ).map( async ([path, info ]) => {
     if( info.triche ) {
         console.log( path, ",", info.bestMatch, ",",info.similarity )
-        let dest =  "similarity/"+ Math.floor( 10*info.similarity) + "/" + path
+        let dest =  "similarity/" + path
         await mkdir(dest , {recursive:true} )
-        copyFile( "result/" + path + ".js", dest + "/" + path + ".js" ) ;
-        copyFile( "result/" + info.bestMatch + ".js", dest + "/" + info.bestMatch + ".js" ) ;
-        
-    } else {
-        let dest =  "similarity/"+ 0 + "/"  
-        await mkdir(dest, {recursive:true} )
-        copyFile( "result/" + path + ".js", dest + "/" + path + ".js") ;
+        copyFile( "result/" + path + ".js", dest + "/" + "__" + path + ".js" ) ;
+        Object.entries( info.all ).map( ([path, sim ] ) => copyFile( "result/" + path + ".js", dest + "/" +  "_" + Math.floor((1 - sim)*10)  + path + ".js" ) );
     }
 })
 
